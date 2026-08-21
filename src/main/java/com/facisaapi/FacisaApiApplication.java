efd4ea80f39a9ca83f1b4493928a1f6bf194fb36
Thales Oliveira <thales.oliveira2567@gmail.com>
2026-08-21 19:46:32 -0300

    first commit

---
+++ src/main/java/com/facisaapi/FacisaApiApplication.java
@@ -0,0 +1,13 @@
+package com.facisaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacisaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacisaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/facisaapi/FacisaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facisaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacisaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
